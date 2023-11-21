ebeb32fce323098ddcc13a1540d51a0b911eddba
Anurag <danurag1906@gmail.com>
2023-11-21 18:30:32 +0530

    added code for deployement

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routers/user.routers.js";
 import authRouter from "./routers/auth.routers.js";
 import listingRouter from "./routers/listing.routers.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json()); //to parse the data coming from the client side
 app.use(cookieParser()); //to access the information from the cookies
@@ -31,6 +34,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist"))); // we will join the dirname form the render server to /client/dist (dist is created after building the vite+ react app)
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+}); //for any address apart from the above three mentioned routes in app.use(), we will redirect the user to index.html
 
 //middleware for error handling
 app.use((err, req, res, next) => {
